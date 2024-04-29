17675e873e3760b362a019c200c348b131fd5d72
Sadiasabit <sadiamahmud71@gmail.com>
2024-04-29 08:15:30 +0600

    array

---
+++ index.js
@@ -0,0 +1,28 @@
+let arr=[];
+arr [0]="tom";
+arr [1]="tuki";
+console.log(arr);
+
+
+let arr1=[];
+arr1 [0]="tom";
+arr1 [1]="tuki";
+arr1.pop ("");
+console.log(arr1);
+
+
+let arr2=[];
+arr2 [0]="tom";
+arr2 [1]="tuki";
+arr2.push ("Tatul");
+console.log(arr2);
+
+
+
+var arr3=["tom","tuki","Tatul","Alish"];
+console.log(arr3.indexOf("Alish"));
+
+
+
+var arr3=["tom","tuki","Tatul","Alish"];
+console.log(arr3.length);
